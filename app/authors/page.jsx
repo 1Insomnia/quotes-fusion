41be488b1139994ed993635011f753b066736c44
Jérémy <jeremy.pro.lp@gmail.com>
2023-12-11 13:56:18 +0100

    init

---
+++ app/authors/page.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Authors - Quotes fusion",
+  description: "Historical quotes from the most influencial figures"
+}
+
+export default function page() {
+  return <div>Authors</div>
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,44 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      primary: {
+        light: "#60bbfc",
+        dark: "#158bc9"
       },
+      foreground: {
+        DEFAULT: "#101112",
+        light: "#3f434a",
+        lighter: "#6e7683"
+      },
+      background: {
+        neutral: {
+          DEFAULT: "#fff",
+          hover: "#f2f3f5",
+          active: "#e6e8ec"
+        },
+        contrast: {
+          DEFAULT: "#101112",
+          hover: "#272a2e",
+          active: "#565d67"
+        }
+      },
+      border: {
+        subtle: "#e6e8ec",
+        moderate: "#ced2d9"
+      }
     },
+    fontFamily: {
+      sans: ["'Inter'", ...defaultTheme.fontFamily.sans],
+      serif: ["'PT Sans'", ...defaultTheme.fontFamily.serif]
+    }
   },
-  plugins: [],
+  plugins: []
 }
